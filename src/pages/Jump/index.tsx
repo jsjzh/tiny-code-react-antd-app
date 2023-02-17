339c5b83ae0c4084a21dbd65a2c7ec259a802c6b
jsjzh <kimimi_king@163.com>
2023-02-17 14:45:21 +0800

    chore: 添加 url 参数解析

--- src/pages/Jump/index.tsx
+++ src/pages/Jump/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { useParams, useSearchParams } from "react-router-dom";
+import { useNavigate, useParams, useSearchParams } from "react-router-dom";
 
 const Jump: React.FC = () => {
   const params = useParams();
   const [searchParams] = useSearchParams();
 
-  console.log(params);
-  console.log(searchParams.get("author"));
-  console.log(searchParams.get("age"));
+  searchParams.forEach((value, key) => {
+    console.log(`key: ${key}`, `value: ${value}`);
+  });
 
   return <>Jump</>;
 };

d143a179307cd28d81115312ebdd62b7ea0c613a
jsjzh <kimimi_king@163.com>
2023-02-21 23:37:52 +0800

    feat: 添加 jsonplaceholder 相关接口

--- README.md
+++ README.md
@@ -21,3 +21,29 @@ utility:lodash+immer
 css: [email protected]
 UX\UI : react-hot-toast react-spinners-kit @heroicons /react react-dnd timeago-react
 vite\typescript\ eslint prettier\ eslint-config-airbn eslint-config-airbnb-typescript
+
+用 `jsonplaceholder.typicode.com` 提供的接口，写一个小应用，纯展示。
+
+```
+/posts 100 posts
+/comments 500 comments
+/albums 100 albums
+/photos 5000 photos
+/todos 200 todos
+/users 10 users
+
+GET /posts
+GET /posts/1
+GET /posts/1/comments
+GET /comments?postId=1
+POST /posts
+PUT /posts/1
+PATCH /posts/1
+DELETE /posts/1
+
+/posts/1/comments
+/albums/1/photos
+/users/1/albums
+/users/1/todos
+/users/1/posts
+```
--- src/api/all.ts
+++ src/api/all.ts
@@ -2,5 +2,14 @@ import createAPI from "@/shared/createAPI";
 
 const api = createAPI(import.meta.env.VITE_JSONPLACEHOLDER_TYPICODE_COM);
 
-export const getUser = (data: { id: number }) =>
-  api.getJSON<D.User>(`/users/${data.id}`);
+export const getPosts = () => api.getJSON<D.Post[]>("/posts");
+
+export const getComments = () => api.getJSON<D.Comment[]>("/comments");
+
+export const getAlbums = () => api.getJSON<D.Album[]>("/albums");
+
+export const getPhotos = () => api.getJSON<D.Photo[]>("/photos");
+
+export const getTodos = () => api.getJSON<D.Todo[]>("/todos");
+
+export const getUsers = () => api.getJSON<D.User[]>("/users");

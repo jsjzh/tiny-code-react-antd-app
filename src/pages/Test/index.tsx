0dbf451cafb83e101473ea49aeccab5ea227b4ed
jsjzh <kimimi_king@163.com>
2023-03-01 21:21:59 +0800

    feat: 增加正常的 useStore 写法

--- src/pages/Test/index.tsx
+++ src/pages/Test/index.tsx
@@ -1,12 +1,14 @@
 import React, { useCallback } from "react";
 
 import PageWrapper from "@/components/PageWrapper";
-import useTestStore from "@/store/test";
+import useTestStore, { ITestStore } from "@/store/test";
 
 interface IProps {}
 
+const mapStateToProps = (state: ITestStore) => state;
+
 const Test: React.FC<IProps> = (props) => {
-  const testStore = useTestStore();
+  const testStore = useTestStore(mapStateToProps);
 
   const bears = useTestStore(useCallback((state) => state.bears, []));
 
@@ -16,8 +18,8 @@ const Test: React.FC<IProps> = (props) => {
         <div>{bears}</div>
         <div>{testStore.bears}</div>
         <div>
-          <button onClick={() => testStore.increase(1)}>+1</button>
-          <button onClick={() => testStore.increase(-1)}>-1</button>
+          <button onClick={() => testStore.change(-1)}>-1</button>
+          <button onClick={() => testStore.change(1)}>+1</button>
         </div>
       </div>
     </PageWrapper>
--- src/store/test.ts
+++ src/store/test.ts
@@ -2,23 +2,34 @@ import { create } from "zustand";
 import { produce } from "immer";
 import { combine, persist } from "zustand/middleware";
 
-// const log = (config: any) => (set, get, api) =>
-//   config(
-//     (args) => {
-//       console.log("  applying", args);
-//       set(args);
-//       console.log("  new state", get());
-//     },
-//     get,
-//     api
-//   );
+// 使用 combine 自动推断类型
+// 感觉不太好，因为还有其他地方会用到 ITestStore 类型
+// const useTestStore = create(
+//   persist(
+//     combine({ bears: 0 }, (set) => ({
+//       increase: (by: number) => set((state) => ({ bears: state.bears + by })),
+//       add: () => set((state) => ({ bears: state.bears + 1 })),
+//     })),
+//     { name: "useTestStore" }
+//   )
+// );
 
-const useTestStore = create(
+interface ITestData {
+  bears: number;
+}
+
+interface ITestFuns {
+  change: (count: number) => void;
+}
+
+export type ITestStore = ITestData & ITestFuns;
+
+const useTestStore = create<ITestStore>()(
   persist(
-    combine({ bears: 0 }, (set) => ({
-      increase: (by: number) => set((state) => ({ bears: state.bears + by })),
-      add: () => set((state) => ({ bears: state.bears + 1 })),
-    })),
+    (set, get, api) => ({
+      bears: 0,
+      change: (count) => set((state) => ({ bears: state.bears + count })),
+    }),
     { name: "useTestStore" }
   )
 );

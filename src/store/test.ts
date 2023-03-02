5099559ce4e7ac8d2942c5a5178e7db3c69ab5e2
jsjzh <kimimi_king@163.com>
2023-03-02 22:18:35 +0800

    feat: add zustand with combine

--- src/store/test.ts
+++ src/store/test.ts
@@ -2,18 +2,6 @@ import { create } from "zustand";
 import { immer } from "zustand/middleware/immer";
 import { combine, devtools, persist } from "zustand/middleware";
 
-// 使用 combine 自动推断类型
-// 感觉不太好，因为还有其他地方会用到 ITestStore 类型
-// const useTestStore = create(
-//   persist(
-//     combine({ bears: 0 }, (set) => ({
-//       increase: (by: number) => set((state) => ({ bears: state.bears + by })),
-//       add: () => set((state) => ({ bears: state.bears + 1 })),
-//     })),
-//     { name: "useTestStore" }
-//   )
-// );
-
 interface ITestData {
   num: number;
 }
@@ -39,5 +27,24 @@ export const useTestStore = create<ITestStore>()(
     { name: "useTestStore" }
   )
 );
+
+// 使用 combine 自动推断类型
+// 感觉不太好，因为还有其他地方会用到 ITestStore 类型
+// const useTestStoreWithCombine = create(
+//   devtools(
+//     persist(
+//       immer(
+//         combine({ num: 0 }, (set) => ({
+//           update: (count: number) =>
+//             set((draft) => {
+//               draft.num = draft.num + count;
+//             }),
+//         }))
+//       ),
+//       { name: "useTestStore" }
+//     ),
+//     { name: "useTestStore" }
+//   )
+// );
 
 export default useTestStore;

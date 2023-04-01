32f8880ac0af92eca9c0bb3972c292d8c3ec7808
jsjzh <kimimi_king@163.com>
2023-04-01 15:53:50 +0800

    feat: 添加 dayjs 配置

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter, BrowserRouter, MemoryRouter } from "react-router-dom";
 import { SWRConfig } from "swr";
+import dayjs from "dayjs";
+import "dayjs/locale/zh-cn";
 
 import App from "@/pages/App";
+
+dayjs.locale("zh-cn");
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   // <React.StrictMode>
